c5d094e08625827256ba4e3edd90ae57459db9b1
Jakub-Svoboda <svo.jakub@seznam.cz>
2017-04-11 17:23:02 +0200

    .

--- trace.cpp
+++ trace.cpp
@@ -1,42 +1,63 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <string.h>
 #include <iostream>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/ip_icmp.h>
 
+#define PORTNUM 33434
 using namespace std;
 
+void getIpv4(struct hostent *server, string address, sockaddr_in * destinationAddress){
+	bzero(destinationAddress, sizeof(destinationAddress));		//null the server address
+	uint32_t addressNum = inet_addr(address.c_str()); 
+	if(addressNum == INADDR_NONE){
+		bcopy((char *) server->h_addr, (char *)&destinationAddress->sin_addr.s_addr, server->h_length);
+	}else{
+		destinationAddress->sin_addr.s_addr=addressNum;
+	}	
+	destinationAddress->sin_family=AF_INET;
+	destinationAddress->sin_port = htons(PORTNUM);
+	bcopy(to_string(addressNum).c_str(), (char *)&destinationAddress->sin_addr.s_addr, strlen(to_string(addressNum).c_str()));
+	
+}
+
 //Check whether the arguments are valid or not and assigns the values to apropriate variables.
-void validateArgs(string *adress, int argc, char* argv[],int * first_ttl, int * max_ttl){
+void validateArgs(string *address, int argc, char* argv[],int * first_ttl, int * max_ttl){
 	if(argc == 3 or argc==5){
 		fprintf(stderr,"Error: Wrong number of arguments %d \n",argc);
 		exit(1);
-	}else if(argc ==2){			//adress only
+	}else if(argc ==2){			//address only
 		*first_ttl=1;
 		*max_ttl=30;
-		*adress = argv[1];
-	}else if(argc == 4){		//adress +1
+		*address = argv[1];
+	}else if(argc == 4){		//address +1
 		if(!strcmp(argv[1],"-f")){					//-f xx ip
 			*first_ttl=atoi(argv[2]);
 			*max_ttl=30;
-			*adress = argv[3];			
+			*address = argv[3];			
 		}else if(!strcmp(argv[1],"-m")){			//-m xx ip
 			*first_ttl=1;
 			*max_ttl=atoi(argv[2]);
-			*adress = argv[3];
+			*address = argv[3];
 		}else{
 			fprintf(stderr,"Parameter error.\n");
 			exit(1);
 		}		
-	}else if(argc == 6){		//adress +2
+	}else if(argc == 6){		//address +2
 		if(!strcmp(argv[1],"-f") and !strcmp(argv[3],"-m")){			//-f xx -m yy ip
 			*first_ttl=atoi(argv[2]);
 			*max_ttl=atoi(argv[4]);
-			*adress = argv[5];	
+			*address = argv[5];	
 		}else if(!strcmp(argv[1],"-m") and !strcmp(argv[3],"-f")){		//-m xx -f yy ip
 			*first_ttl=atoi(argv[4]);
 			*max_ttl=atoi(argv[2]);
-			*adress = argv[5];	
+			*address = argv[5];	
 		}else{
 			fprintf(stderr,"Parameter error.\n");
 			exit(1);
@@ -53,8 +74,56 @@ int main(int argc, char* argv[]){
 		exit(1);		
 	}
 	int first_ttl, max_ttl;
-	string adress;
-	validateArgs(&adress,argc,argv, &first_ttl, &max_ttl);
+	string address;
+	struct hostent *server = NULL;	
+	validateArgs(&address,argc,argv, &first_ttl, &max_ttl);
+	struct sockaddr_in destinationAddress; 
+	//struct sockaddr_in6 destinationAddress6;
+	if(string::npos!=address.find('.')){				//ipv4
+		getIpv4(server, address, &destinationAddress);
+	}else if(string::npos!=address.find(':')) {											//ipv6
+		cout<<"ipv6"<<endl;
+	}else{
+		cout<<"translation needed"<<endl;
+		exit(1);
+	}
+	uint32_t clientSocket;
+	if ((clientSocket=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) <=0){
+        fprintf(stderr,"Socket failed to create.\n");
+        exit(EXIT_FAILURE);
+    }
+		
+	uint32_t slen=sizeof(destinationAddress);
 	
+	struct icmphdr packet;
+	packet.type = ICMP_ECHO;
+	packet.code = 0;
+	packet.un.echo.id=getpid();
+	packet.un.echo.sequence=1;
+	packet.checksum=0;
+	int val=255;
+	setsockopt(clientSocket, SOL_IP, IP_TTL, &val, sizeof(val));
+	
+	
+	
+	//send the message
+	if (sendto(clientSocket, &packet, sizeof(packet) , 0 , (struct sockaddr *) &destinationAddress, slen) <= 0){
+		fprintf(stderr,"sendto() failed with error code \n");
+		exit(-1);
+	}
+	
+	
+	//receive
+	char buf[1000];
+	memset(buf,'\0', 1000);
+	int bytes;
+	
+	if ((bytes=recvfrom(clientSocket, buf, 1000, 0,  (struct sockaddr *)&destinationAddress, &slen)) <= 0){
+		fprintf(stderr,"recvfrom() failed. \n");
+		exit(-1);
+	}else{
+		cout<<bytes<<endl;
+	}
+
 	
 }
