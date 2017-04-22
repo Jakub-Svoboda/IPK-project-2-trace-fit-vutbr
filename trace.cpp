ee6213c60f39fb201da71a555ea5dab2366e8ee2
Jakub-Svoboda <svo.jakub@seznam.cz>
2017-04-22 15:53:46 +0200

    .

--- trace.cpp
+++ trace.cpp
@@ -110,6 +110,7 @@ int main(int argc, char* argv[]){
 	setsockopt(clientSocket, SOL_IP, SO_RCVTIMEO, &val, sizeof(val));
 
 	//send the message
+	cout<<first_ttl;
 	if ((sendto(clientSocket, &packet, sizeof(packet) , 0 , (struct sockaddr *) &destinationAddress, slen)) <= 0){
 		fprintf(stderr,"sendto() failed with error code %d\n",errno);
 		exit(-1);
@@ -172,7 +173,7 @@ int main(int argc, char* argv[]){
 		first_ttl++;
 		setsockopt(clientSocket, IPPROTO_IP, IP_TTL, &first_ttl, sizeof(first_ttl));
 		//send the message
-		cout <<"sending with ttl: " <<first_ttl<<endl;
+		cout <<first_ttl;
 		if ((sendto(clientSocket, &packet, sizeof(packet) , 0 , (struct sockaddr *) &destinationAddress, slen)) <= 0){
 			fprintf(stderr,"sendto() failed with error code %d\n",errno);
 			exit(-1);
