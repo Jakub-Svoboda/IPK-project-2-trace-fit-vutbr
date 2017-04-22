d8d226e95434fb3e688ff78e80d70a1e8f2fa1da
Jakub-Svoboda <svo.jakub@seznam.cz>
2017-04-22 22:25:30 +0200

    .

--- trace.cpp
+++ trace.cpp
@@ -156,9 +156,9 @@ int main(int argc, char* argv[]){
 		messageHeader.msg_controllen = sizeof(buf);	
 
 		if(isIt6){
-			setsockopt(socket6, IPPROTO_IP, IP_TTL, &first_ttl, sizeof(first_ttl));
+			setsockopt(socket6, IPPROTO_IPV6, IPV6_UNICAST_HOPS, &first_ttl, sizeof(first_ttl));
 		}else{
-			setsockopt(clientSocket, IPPROTO_IPV6, IPV6_UNICAST_HOPS, &first_ttl, sizeof(first_ttl));
+			setsockopt(clientSocket, IPPROTO_IP, IP_TTL, &first_ttl, sizeof(first_ttl));
 		}	
 		
 		//send the message
